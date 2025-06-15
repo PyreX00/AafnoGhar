6d10cd8c5939939c9423ce0a9725d37467277159
PyreX00 <Subedianish2022@gmail.com>
2025-06-15 14:43:33 +0545

    fixed login modal

---
+++ backend/users/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.base_user import BaseUserManager
+# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password= None , **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self, email, password = None, **extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        return self.create_user(email,password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length = 200, unique = True)
+    birthday = models.DateField(null = True, blank = True)
+    username = models.CharField(max_length=200, null = True, blank = True)
+    role = models.ForeignKey(Group, on_delete=models.PROTECT)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = []
+    
